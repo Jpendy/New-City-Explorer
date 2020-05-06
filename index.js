4cd8bbcdbaf4a4d1dc35e4e0d2be22f0f853cf5a
Jpendy <Jpendy256@gmail.com>
2020-05-06 14:52:10 -0700

    added some api keys

--- index.js
+++ index.js
@@ -4,17 +4,33 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const request = require('superagent');
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/location', (req, res) => {
-    res.json({ hello: 'location' });
+const locationData = require('./data/geo.json');
+const weatherData = require('./data/weather.json');
+
+const { mungeWeather } = require('./utils.js');
+const { mungeLocation } = require('./utils.js');
+
+
+app.get('/location', async(req, res) => {
+
+    const data = request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_Key}&q=${req.query.search}&format=json`);
+    const mungedData = mungeLocation(data);
+    res.json({ mungedData });
 });
 
+
+
 app.get('/weather', (req, res) => {
-    res.json({ hello: 'weather' });
+
+    const mungedData = mungeWeather(weatherData);
+    res.json({ mungedData });
 });
 
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`); });
---
+++ utils.js
@@ -0,0 +1,37 @@
+
+function mungeLocation(locationData){
+    try {const firstitem = locationData[0];
+
+        return {
+            formatted_query: firstitem.display_name,
+            latitude: firstitem.lat,
+            longitude: firstitem.lon
+        };
+    } catch (e) {
+        return {};
+    }    
+}
+
+
+
+function mungeWeather(weatherData){
+    
+    try {
+        const transformedData = weatherData.data.map(item => {
+            return {
+                forecast: item.weather.description,
+                time: item.datetime
+            };
+        
+        });
+        return transformedData.slice(0, 8);
+    } 
+    catch (e) {
+        return [{}];    
+    }    
+}
+
+module.exports = {
+    mungeLocation,
+    mungeWeather
+};
